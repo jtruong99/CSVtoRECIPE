f0931ac582725f39a976a2a97d8883bb3c7a123b
brettbarbaro <brettbarbaro@gmail.com>
2016-07-11 14:15:01 -0700

    Add files via upload

---
+++ CSVtoRECIPE.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Friday, July 8, 2016
+@authors: brett barbaro, ludovic autin
+"""
+
+# fill in missing data
+#   Ingredients from proteomics:
+#     Needs name (the name in the data you have)
+#     Needs unique ID e.g. uniprot or ncbi (synthetic cell or http://wholecelldb.stanford.edu)
+#     Needs the copy number of concentration (e.g. from mass spec, or http://wholecelldb.stanford.edu/ or david estimation from his painting...)
+#     Fetch the sequence
+#     Fetch or calculate  the molecular weight
+#     From sequence retrieve a template from the PDB. Keep e-value and score…
+#     Retrieve the multimer states for the structure using the PDB (biological assembly).
+#     From sequence retrieve the localisation if unknow (e.g. cytoplasm, membrane etc,...)
+#       http://www.ncbi.nlm.nih.gov/CBBresearch/Lu/subcellular/
+#       http://www.imtech.res.in/raghava/pslpred/
+#
+#     From sequence and template from the PDB build an homology model (e.g. MODELLER or server license for MODELLER is MODELAJE in upper case)
+#     If localisation is inner/outer membrane predict/retrieve the orientation of the structure  using http://opm.phar.umich.edu
+#     Once a structure is known,
+#       Load and center the atoms to the origin
+#       apply k-means clustering based on number of atoms.
+#       Generate a coarse molecular surface and save it as collada 1.4
+#     Generate a recipe file in json.
+#
+#     Note:
+#     http://wholecellkb.stanford.edu/list/Mgenitalium/ProteinComplex contains the mutlimeric states off all  proteins from mycoplasma genitalium. All entry are defined according the gene name (MG_001->MG_526). However for each gene there is a link to the uniprort.
+#     Example code are in the github repo of autopack in the script folder
+#     For ncbi access the fasta sequence can be done with
+#     http://www.ncbi.nlm.nih.gov/protein/296456015?report=fasta&log$=seqview&format=text#
+
+#   export results as json
